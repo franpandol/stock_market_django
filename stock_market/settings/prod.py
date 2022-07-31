fd1159dc87b9e9abe851583013413a4343baf14c
FranPandol <pandol.francisco@gmail.com>
2022-07-31 14:18:10 -0300

    Refactor migrations for authentication

--- stock_market/settings/prod.py
+++ stock_market/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '.env.prod')
 load_dotenv(dotenv_path, verbose=True)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
